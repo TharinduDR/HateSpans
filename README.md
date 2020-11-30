d723462b38d39b98b4c3afa4161bbd95de7cb5d0
TharinduDR <rhtdranasinghe@gmail.com>
2020-11-30 16:35:34 +0000

    001: Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HateSpans
+
+We provide state-of-the-art models to detect toxic spans in text.
---
+++ examples/english/english_experiment.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import train_test_split
+
+from examples.english.transformer_configs import transformer_config, MODEL_TYPE, MODEL_NAME
+from hatespans.algo.hate_spans_model import HateSpansModel
+from hatespans.algo.preprocess import read_datafile, format_data
+
+train = read_datafile('data/tsd_train.csv')
+dev = read_datafile('data/tsd_trial.csv')
+
+train_df = format_data(train)
+# train_df.to_csv("train_1.csv", sep='\t', encoding='utf-8', index=False)
+
+model = HateSpansModel(MODEL_TYPE, MODEL_NAME, args=transformer_config)
+
+if transformer_config["evaluate_during_training"]:
+    train_df, eval_df = train_test_split(train, test_size=0.1,  shuffle=False)
+    model.train_model(train_df, eval_df=eval_df)
+
+else:
+    model.train_model(train_df)
+
+
+
