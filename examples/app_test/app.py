e69e54a9af2ea1300d9b7579518f31ff77526fc8
TharinduDR <rhtdranasinghe@gmail.com>
2020-12-04 19:55:02 +0000

    004: Added new prediction function

--- examples/app_test/app.py
+++ examples/app_test/app.py
@@ -2,3 +2,7 @@ from hatespans.app.hate_spans_app import HateSpansApp
 
 app = HateSpansApp("small", use_cuda=False)
 print(app.predict_hate_spans("You motherfucking cunt. I will kill you", spans=True))
+
+tokens = app.predict_tokens("You motherfucking cunt. I will kill you")
+for token in tokens:
+    print(token.text, token.is_toxic)
